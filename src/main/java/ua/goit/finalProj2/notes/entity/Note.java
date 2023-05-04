cb0d14babccd42cef916abef222fad29b5ff729d
SellerRyba <sellerryba@gmail.com>
2023-05-04 19:12:35 +0300

    create new migration, entity and repository

---
+++ src/main/java/ua/goit/finalProj2/notes/entity/Note.java
@@ -0,0 +1,33 @@
+package ua.goit.finalProj2.notes.entity;
+
+import lombok.Data;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "notes")
+public class Note {
+
+    @Id
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "content")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "access_type")
+    private AccessType accessType;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+}
