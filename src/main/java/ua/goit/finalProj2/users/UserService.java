e52488cbf2ae5793088f97735ed1cebd9a97ac8f
YourAloneFriend <vasya202003@gmail.com>
2023-05-06 11:17:40 +0300

    Authentication commit

--- src/main/java/ua/goit/finalProj2/users/UserService.java
+++ src/main/java/ua/goit/finalProj2/users/UserService.java
@@ -3,11 +3,12 @@ package ua.goit.finalProj2.users;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import ua.goit.finalProj2.users.form_common.AuthenticationException;
 import ua.goit.finalProj2.users.form_common.UserDAO;
-import ua.goit.finalProj2.users.registration.RegistrationException;
 
-import static ua.goit.finalProj2.users.form_common.UserMapper.toUser;
+import static ua.goit.finalProj2.users.form_common.UserValidate.*;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +17,19 @@ public class UserService {
     @Autowired
     private final UserRepository repository;
 
-    public void createUser(UserDAO userDAO) throws RegistrationException {
-        repository.save(toUser(userDAO));
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
+
+    public void createUser(UserDAO userDAO) throws AuthenticationException{
+        validateUserAuthentication(userDAO);
+        userDAO.setPassword(passwordEncoder.encode(userDAO.getPassword()));
+        repository.create(userDAO);
+    }
+
+    public User getUserByEmail(UserDAO userDAO) throws AuthenticationException{
+        validateUserRegister(userDAO);
+        userDAO.setPassword(passwordEncoder.encode(userDAO.getPassword()));
+        return repository.getUserByEmail(userDAO);
     }
 }
--- src/main/java/ua/goit/finalProj2/users/registration/RegistrationException.java
+++
@@ -1,8 +0,0 @@
-package ua.goit.finalProj2.users.registration;
-
-public class RegistrationException extends Exception{
-
-    RegistrationException(String message){
-        super(message);
-    }
-}
