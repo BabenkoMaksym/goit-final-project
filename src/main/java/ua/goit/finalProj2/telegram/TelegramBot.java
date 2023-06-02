0b55dc240b5664cc81b6e865b4c995463432211a
BabenkoMaksym <Maksym.Babenko.bbn@gmail.com>
2023-06-02 13:45:01 +0200

    add menu

--- src/main/java/ua/goit/finalProj2/telegram/TelegramBot.java
+++ src/main/java/ua/goit/finalProj2/telegram/TelegramBot.java
@@ -4,8 +4,15 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
@@ -16,6 +23,19 @@ public class TelegramBot extends TelegramLongPollingBot {
     public TelegramBot() {
         super("6220505870:AAG9f6zwSUWGkVUB6hGPYJOW7dz1CwAvAP0");
         this.config = new BotConfig();
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "start bot"));
+        listOfCommands.add(new BotCommand("/create", "create new note"));
+        listOfCommands.add(new BotCommand("/search", "search notes"));
+        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listOfCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listOfCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
